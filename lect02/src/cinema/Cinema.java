e3952b1453de326fc0c6d3f443141791bdc259e4
umarins <umarins@ku.th>
2021-01-14 13:00:34 +0700

    Project Cinema

---
+++ lect02/src/cinema/Cinema.java
@@ -0,0 +1,16 @@
+package cinema;
+
+public class Cinema {
+
+  void soldTickets() {
+    // คำนวณรายได้จากการขายตั๋ว
+  } 
+
+  void showSeats(int numRow, int numCol) {
+    // แสดงข้อมูลที่นั่ง
+  }
+  public static void main(String[] args) {
+    showSeats( 7, 8);
+    soldTickets();  
+  }
+}
