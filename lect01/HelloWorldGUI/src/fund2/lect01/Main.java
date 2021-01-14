e3fcc5a2687616e4351a563dd8b197d086b1ed25
umarins <umarins@ku.th>
2021-01-14 10:52:20 +0700

    lect02: tamjung chatbot

--- lect01/HelloWorldGUI/src/fund2/lect01/Main.java
+++ lect01/HelloWorldGUI/src/fund2/lect01/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Hello, Java!");
-        frame.add(new HelloJava1());
+        JLabel label = new JLabel("Hello Fund2!");
+        frame.add(label);
+        // frame.add(new HelloJava1());
         frame.setSize(300, 300);
         frame.setVisible(true);
     }
---
+++ lect02/src/bot/Tamjung.java
@@ -0,0 +1,43 @@
+package bot;
+
+import java.util.Scanner;
+
+class Tamjung {
+  private Scanner scanner;
+  private String botName;
+  private int createdYear;
+
+  Tamjung(String botName, int createdYear) {
+    this.scanner = new Scanner(System.in);
+    this.botName = botName;
+    this.createdYear = createdYear;
+  }
+   
+  void greeting() {
+    // การรับข้อมูลจากคีย์บอร์ด 
+    
+  }
+  
+  void guessAge() {
+    // การเขียน arithmetic expression  
+
+  }
+
+  void countNumber() {
+    // ใช้ while-loop
+
+  }
+
+  void quiz() {
+    // ใช้ switch statement
+
+  }
+
+  public static void main(String[] args) {
+    Tamjung bot = new Tamjung("tamjung", 2020);
+    bot.greeting(); 
+    bot.guessAge();
+    bot.countNumber();
+    bot.quiz(); 
+  }
+}
