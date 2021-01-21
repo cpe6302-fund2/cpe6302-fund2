baa7963f6dcf4d2e16ad3c00b9d78c284c548188
kulwadeeku <kulwadee.a@outlook.com>
2021-01-21 13:20:38 +0700

    solution for week2

--- lect02/src/bot/Tamjung.java
+++ lect02/src/bot/Tamjung.java
@@ -1,3 +1,8 @@
+// 3 Collaborators:
+// 1. Student ID: Firstname Surname
+// 2.
+// 3.
+
 package bot;
 
 import java.util.Scanner;
@@ -15,22 +20,64 @@ class Tamjung {
    
   void greeting() {
     // การรับข้อมูลจากคีย์บอร์ด 
-    
+    System.out.println("สวัสดี! ฉันชื่อ " + this.botName);
+    System.out.println("ฉันถูกสร้างขึ้นเมื่อปี " + this.createdYear);
+    System.out.println("เธอชื่ออะไร?");
+    System.out.print("> ");
+
+    String name = this.scanner.nextLine();
+    System.out.println("สวัสดี " + name + "!");
   }
   
   void guessAge() {
     // การเขียน arithmetic expression  
+    System.out.println("ฉันจะเดาว่าเธออายุเท่าไหร่");
+    System.out.println("ช่วยบอก เศษที่ได้จากการหารอายุของเธอด้วย 3 5 และ 7");
 
+    System.out.print("> ");
+    int rem3 = this.scanner.nextInt();
+    System.out.print("> ");
+    int rem5 = this.scanner.nextInt();
+    System.out.print("> ");
+    int rem7 = this.scanner.nextInt();
+
+    int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+    System.out.println("เธออายุ " + age + ": เป็นช่วงอายุที่น่าสนุกมากของชีวิตเลย!");
   }
 
   void countNumber() {
     // ใช้ while-loop
+    System.out.println("เธอต้องการให้ฉันนับถึงเลขอะไร?");
+    System.out.print("> ");
+    int num = this.scanner.nextInt();
+    int i = 1;
+    do {
+      System.out.println(i + "!");
+      i++;
+    } while (i <= num);
 
   }
 
   void quiz() {
-    
+    System.out.println("ฉันขอทดสอบความรู้เกี่ยวกับการเขียนโปรแกรมของเธอหน่อยนะ");
+    System.out.println("ทำไมเราถึงต้องใช้ method?");
+    System.out.println("ก. เพื่อรันคำสั่งซ้ำหลาย ๆ ครั้ง");
+    System.out.println("ข. เพื่อแบ่งโปรแกรมออกเป็น subroutine เล็กๆ หลายๆ รูทีน");
+    System.out.println("ค. เพื่อวัดเวลาการรันโปรแกรม");
+    System.out.println("ง. เพื่ออินเตอร์รัพการทำงานของโปรแกรม");
 
+
+    String answer;
+    System.out.print("> ");
+    answer = this.scanner.next(); 
+    while (!answer.equals("ข")) 
+    {
+      System.out.println("ผิด! ลองใหม่อีกครั้งนะ");
+      System.out.print("> ");
+      answer = this.scanner.nextLine(); 
+
+    } 
+    System.out.println("ยินดีด้วย เธอตอบถูก!");
   }
 
   public static void main(String[] args) {
--- lect02/src/cinema/Cinema.java
+++ lect02/src/cinema/Cinema.java
@@ -1,17 +1,71 @@
+// 3 Collaborators
+// 1. Student ID FirstName LastName
+// 2.
+// 3.
+
 package cinema;
+import java.util.Scanner;
 
 public class Cinema {
+  final static Scanner scanner = new Scanner(System.in);
 
-  void soldTickets() {
+  static void soldTickets() {
     // คำนวณรายได้จากการขายตั๋ว
+    int numRow;
+    int seatPerRow;
+    System.out.println("จำนวน แถวที่นั่ง :");
+    System.out.print("> ");
+    numRow = scanner.nextInt();
+    
+    System.out.println("จำนวนที่นั่งในแต่ละแถว : ");
+    System.out.print("> ");
+    seatPerRow = scanner.nextInt();
+    
+    int numSeats = numRow * seatPerRow;
+    int income;
+
+    if (numSeats < 60) {
+      income = numSeats * 100;
+    } else {
+      // > 60
+      int front = numRow / 2;
+      int back = numRow - front;
+
+      income = front * seatPerRow * 100 + back * seatPerRow * 80;
+    }
+
+    System.out.println("รายได้จากการจำหน่ายตั๋ว :");
+    System.out.println(income + " บาท");
   } 
 
-  void showSeats(int numRow, int numCol) {
+  static void showSeats(int numRow, int numCol) {
     // แสดงข้อมูลที่นั่ง
+    System.out.println("Cinema:");
+    System.out.print("  ");
+    for (int i=1; i<=numCol; i++) {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+
+    char[][] seats = new char[numRow][numCol];
+
+    for (int i = 0; i < numRow; i++) {
+      for (int j = 0; j < numCol; j++) {
+        seats[i][j] = 'S';
+      }
+    }
+
+    for (int i = 0; i < numRow; i++) {
+      System.out.print(i + 1 + " ");
+      for (int j = 0; j < numCol; j++) {
+        System.out.print(seats[i][j] + " ");
+      }
+      System.out.println();
+    }
   }
+
   public static void main(String[] args) {
     showSeats( 7, 8);
     soldTickets();  
-    
   }
 }
