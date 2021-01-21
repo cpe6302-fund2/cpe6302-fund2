6177457bd2be28ab971cae3a12523f7beeb1a30b
kulwadeeku <77065282+kulwadeeku@users.noreply.github.com>
2021-01-21 13:39:01 +0700

    Update Problem3_1.java

--- lect03/src/Problem3_1.java
+++ lect03/src/Problem3_1.java
@@ -1,3 +1,12 @@
+// Student ID:
+// First Name:
+// Last Name:
+//
+// Team Members:
+// 1. <student id> <first name> <last name>
+// 2.
+// 3.
+
 public class Problem3_1 {
     public static void main(String[] args) {
         // TODO
