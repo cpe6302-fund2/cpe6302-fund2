b85c7d865a0affab6d49d36e62dc766e64cb6ad4
kako00053 <77090542+kako00053@users.noreply.github.com>
2021-02-11 18:12:09 +0700

    Update Card.java

--- lect04/Card.java
+++ lect04/Card.java
@@ -6,12 +6,17 @@ public class Card {
   public static final String[] SUITS = {
     "Clubs", "Diamonds", "Hearts", "Spades"};
 
+  public enum Rank{
+  ACE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,JACK,QUEENKING}
+
+   public enum Suit{
+  CLUBS,DIAMONDS,HEARTS,SPADES}
+
   private final int rank;
   private final int suit;
   
   // Queens of Spades: rank = 12, suit=3
   // Five of Diamonds: rank = 5, suit=1
-
   public Card(int rank, int suit) {
     this.rank = rank;
     this.suit = suit;
@@ -20,6 +25,7 @@ public class Card {
   //override method toString of java.lang.Object
   public String toString() {
     String s = RANKS[this.rank] + " of " + SUITS[this.suit];
+    String s = this.rank + " of " + SUITS[this.suit];
     return s;
   }
 
@@ -37,9 +43,18 @@ public class Card {
     }
     if (this.rank < that.rank) {
       return -1;
+    int cmp_suit = this.suit.compareTo(that.suit);
+    int cmp_suit = this.suit.compareTo(that.rank);
+
+    if(cmp_suit == 0 $$ cmp_rank == 0){
+      return 0;
     }
     if (this.rank > that.rank) {
       return 1;
+    if (cmp.suit != 0) {
+      return cmp_suit;
+    else
+      return cmp_rank;
     }
     return 0;
   }
@@ -47,12 +62,9 @@ public class Card {
   public int getRank() {
     return this.rank;
   }
-
   // accessor method
   public int getSuit() {
     return this.suit;
   }
-
   // no setter method due to 'immutable' 
-
 }
